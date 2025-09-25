16ebcf6134fa4f51ad81ad66a1182dc65ffb8e7f
YZZlD <max.verhulst@hotmail.com>
2025-09-25 17:58:22 -0600

    Implemented dynamic ranges with dynamic sequence length

--- DisplayASequenceOfUniqueRandomValues/Program.cs
+++ DisplayASequenceOfUniqueRandomValues/Program.cs
@@ -6,25 +6,35 @@ namespace DisplayASequenceOfUniqueRandomValues
     {
         private static void DisplayASequenceOfUniqueRandomValues(int lowerBound, int upperBound, int sequenceLength)
         {
-            int[] sequence = new int[upperBound - lowerBound];
             Random rand = new Random();
 
+            //Determine the size of the array based off of the range provided for the random numbers.
+            int[] sequence = new int[upperBound - lowerBound + 1];
+
+            //Populate the array with the numbers within range lowerBound-upperBound inclusive.
             for (int i = 0; i < sequence.Length; i++)
             {
                 sequence[i] = lowerBound + i;
             }
 
-            for (int x = sequenceLength; x >= 1; x--)
+            //This generates a random index within the array working as a random number within the range.
+            //Display the number based on the index then set the value of the index to the current "last" value in the array (this achived through restricting possible indices in random).
+            //Then we increment x which in turn reduces the number of accessible indices within the array "shortening" it.
+            for (int x = 0; x < sequenceLength; x++)
             {
-                int index = rand.Next(x);
+                int index = rand.Next(upperBound - x);
                 Console.Write(sequence[index] + " ");
-                sequence[index] = sequence[x];
+                sequence[index] = sequence[upperBound - x - 1];
+
+                //This is debug line to check array status.
+                // sequence[upperBound - x - 1] = 0;
+                // Console.WriteLine(string.Join(' ', sequence));
             }
         }
 
         public static void Main(String[] args)
         {
-            DisplayASequenceOfUniqueRandomValues(1, 100, 100);
+            DisplayASequenceOfUniqueRandomValues(1, 50, 40);
         }
     }
 }
