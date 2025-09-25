feee44fb377ebd5eefaed6e8d22e0b56fe42fcdb
YZZlD <max.verhulst@hotmail.com>
2025-09-25 17:08:57 -0600

    Initial implementation of solution

--- DisplayASequenceOfUniqueRandomValues/Program.cs
+++ DisplayASequenceOfUniqueRandomValues/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DisplayASequenceOfUniqueRandomValues
+{
+    class Program
+    {
+        private static void DisplayASequenceOfUniqueRandomValues(int lowerBound, int upperBound, int sequenceLength)
+        {
+            int[] sequence = new int[upperBound - lowerBound];
+            Random rand = new Random();
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                sequence[i] = lowerBound + i;
+            }
+
+            for (int x = sequenceLength; x >= 1; x--)
+            {
+                int index = rand.Next(x);
+                Console.Write(sequence[index] + " ");
+                sequence[index] = sequence[x];
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            DisplayASequenceOfUniqueRandomValues(1, 100, 100);
+        }
+    }
+}
